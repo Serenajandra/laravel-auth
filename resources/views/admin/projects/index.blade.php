39f4ecab12b7a0e1373b2f974dcc204805037f26
Serenajandra <serena.jandra@gmail.com>
2023-01-10 18:13:50 +0100

    first commit

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="container">
+        <h2 class="bg-success">Lista dei progetti</h2>
+        <p class="bg-primary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolor repellat, non
+            perferendis provident
+            molestiae quae expedita, nihil esse quas adipisci culpa beatae possimus impedit deleniti eaque. Maxime,
+            necessitatibus maiores?</p>
+    </div>
+@endsection
