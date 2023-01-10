3606eb59c2ccf7db7d7eb45629055afb9a42639f
Serenajandra <serena.jandra@gmail.com>
2023-01-10 18:36:17 +0100

    pagina index impostata

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,9 +2,34 @@
 @section('content')
     <div class="container">
         <h2 class="bg-success">Lista dei progetti</h2>
-        <p class="bg-primary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolor repellat, non
-            perferendis provident
-            molestiae quae expedita, nihil esse quas adipisci culpa beatae possimus impedit deleniti eaque. Maxime,
-            necessitatibus maiores?</p>
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Data di creazione</th>
+                            <th scope="col">Argomenti</th>
+                            <th scope="col">Azioni</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($projects as $project)
+                            <tr>
+                                <th scope="row">{{ $project->title }}</th>
+                                <td>{{ $project->created_at }}</td>
+                                <td>{{ $project->subject }}</td>
+                                <td>
+                                    <a class="btn btn-success" href="{{ route('admin.projects.show', $project->slug) }}">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     </div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,62 +16,61 @@
 
 <body>
     <div>
-        <header>
-            {{-- Header --}}
-            <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
-                <div class="row justify-content-between">
-                    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolFolio</a>
-                    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
-                        data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div class="navbar-nav">
-                    <div class="nav-item text-nowrap ms-2">
-                        <a class="nav-link" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
+        {{-- Header --}}
+        <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-2 shadow">
+            <div class="row justify-content-between">
+                <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">BoolFolio</a>
+                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="navbar-nav">
+                <div class="nav-item text-nowrap ms-2">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </div>
-            </header>
-            {{-- /Header --}}
-
-            <div class="container-fluid vh-100">
-                <div class="row h-100">
-                    {{-- Sidebar --}}
-                    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
-                        <div class="position-sticky pt-3">
-                            <ul class="nav flex-column">
-                                <li class="nav-item">
-                                    <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.dashboard' ? 'bg-secondary' : '' }}"
-                                        href="{{ route('admin.dashboard') }}">
-                                        <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i>
-                                        Dashboard
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.projects.index' ? 'bg-secondary' : '' }}"
-                                        href="{{ route('admin.projects.index') }}"> <i class="fa-solid fa-list"></i>
-                                        Projects
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    {{-- /Sidebar --}}
-
+            </div>
         </header>
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            @yield('content')
-        </main>
-        <footer>footer</footer>
-    </div>
+        {{-- /Header --}}
+
+        <div class="container-fluid vh-100">
+            <div class="row h-100">
+                {{-- Sidebar --}}
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-dark navbar-dark sidebar collapse">
+                    <div class="position-sticky pt-3">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.dashboard') }}">
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() === 'admin.projects.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.projects.index') }}"> <i class="fa-solid fa-list"></i>
+                                    Projects
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                {{-- /Sidebar --}}
+
+                </header>
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    @yield('content')
+                </main>
+                <footer>footer</footer>
+            </div>
 </body>
 
 </html>
